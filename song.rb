65d580dafef0183c0c3dc18f337f38ad96b80459
Alex Tyson <atyson@bowdoin.edu>
2021-05-12 21:41:55 -0400

    created wav file of song

--- song.rb
+++ song.rb
@@ -3,7 +3,7 @@ use_bpm 120
 chords = (ring 1,4,2,5)
 
 live_loop :piano do
-  ##| stop
+  stop
   use_synth :piano
   tick
   4.times do
@@ -13,66 +13,74 @@ live_loop :piano do
 end
 
 live_loop :cymbal1 do
-  ##| stop
+  stop
   sample :drum_cymbal_closed if spread(7,16).tick
   sleep 0.5
 end
 
 live_loop :snare do
-  ##| stop
+  stop
   sync :cymbal1
   sample :drum_snare_hard if spread(4,16).tick
 end
 
 live_loop :bd do
-  ##| stop
+  stop
   sync :snare
   sample :bd_ada if spread(10,16).tick
   sleep 0.25
 end
 
 live_loop :cymbal2 do
-  ##| stop
+  stop
   sync :bd
   sample :drum_cymbal_pedal if spread(4,16).tick
 end
 
+live_loop :more_drums do
+  stop
+  sync :bd
+  sample :drum_tom_mid_soft if spread(4,16).tick
+end
+
 live_loop :sounds do
-  ##| stop
+  stop
   sync :piano
   sample :ambi_swoosh, amp: 3
   sleep 5
 end
 
-live_loop :more_drums do
-  ##| stop
-  sync :bd
-  sample :drum_tom_hi_soft if spread(9,16).tick
-  sleep 0.25
-end
-
 live_loop :beeps do
-  ##| stop
+  stop
   use_synth :dtri
   play (chord :A, :major).choose, release: 0.25, cutoff: rrand(60, 110), amp: 5
   sleep [0.5,1,2,3,4].choose
 end
 
 live_loop :hum do
-  ##| stop
+  stop
   sync :bd
   sample :ambi_glass_hum, amp: 0.5
   sleep 5
 end
 
 live_loop :melody do
-  ##| stop
-  use_synth :kalimba
-  tick
-  play [45,47,49,50,52,54,56,57].choose, amp: 3
-  sleep 0.25
+  stop
+  with_fx :reverb do
+    use_synth :kalimba
+    play (scale :A, :major_pentatonic).choose, amp: 7, cutoff: rrand(70,130)
+    sleep [1,2].choose
+  end
 end
 
+live_loop :laser do
+  ##| stop
+  sync :bd
+  with_fx :echo do
+    synth :tb303, note: (octs :a5, 3).tick, release: 0.125
+    sleep 8
+  end
+end
 
 
 
